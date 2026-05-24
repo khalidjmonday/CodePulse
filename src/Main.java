77ca4ee9d1e06e5b0c29d692604f014e7fa2d2b7
khalidjmonday <yashaswi5025@gmail.com>
2026-05-24 18:49:53 +0530

    Initial CodePulse setup

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("        CODEPULSE v0.1");
+        System.out.println(" Smart Developer Analyzer Tool");
+        System.out.println("=================================");
+
+        System.out.println("System Initialized Successfully.");
+    }
+}
